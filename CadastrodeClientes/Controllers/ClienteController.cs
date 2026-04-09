7db0cad67cbc59898e9e59e8ea5a4f8fd31419f7
Edgard <edgardpimentaa02@gmail.com>
2026-04-09 02:12:25 -0300

    feat(controller): update ClienteController with improved request handling

--- CadastrodeClientes/Controllers/ClienteController.cs
+++ CadastrodeClientes/Controllers/ClienteController.cs
@@ -1,40 +1,73 @@
 ﻿using CadastrodeClientes.Entities;
 using CadastrodeClientes.Enums;
+using CadastrodeClientes.Validations;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace CadastrodeClientes.Controllers
 {
     public class ClienteController
     {
-         public void CadastrarClientes()
-        {
+       
+        public static void GerenciarProdutos()
+        { 
+
             Console.WriteLine("\nCADASTRO DE CLIENTES:\n");
-
-            //Criando objeto cliente e solicitando os dados do cliente
-            var cliente = new Cliente();
-            Cliente c = cliente; 
+            Console.WriteLine();
 
 
-            Console.WriteLine("Informe o Nome do Cliente:", cliente.Nome);
+            string nome;
+
+            do
+            {
+                Console.Write("Informe o Nome do Cliente:");
+                nome = Console.ReadLine() ?? string.Empty;
+                if (string.IsNullOrWhiteSpace(nome))
+                {
+                    Console.WriteLine("Nome inválido. Tente novamente.\n");
+                }
+
+            } while (string.IsNullOrWhiteSpace(nome));
+
 
             Console.WriteLine("Informe o Cpf do cliente....:");
+            var cpf = Console.ReadLine() ?? string.Empty;
+
+
+            Console.WriteLine("Informe o Email do cliente....:");
+            var email = Console.ReadLine() ?? string.Empty;
+
+
+            try
+            {
+                var cliente = new Cliente(nome,email,cpf);
+
+                Console.WriteLine("\n Cliente cadastrado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\n Erro ao cadastrar cliente: {ex.Message}");
+            }
+
 
             Console.WriteLine("Data de cadastro (dd/MM/yyyy)....:");
-            cliente.DataCadastro = DateOnly.Parse(Console.ReadLine());
+
+
 
             Console.WriteLine("Informe o tipo de Cliente....:");
-            cliente.TipoDeCliente = (TipoDeCliente) int.Parse(Console.ReadLine());
+
             Console.WriteLine("\t(1) Pessoa Fisica");
             Console.WriteLine("\t(2) Pessoa Juridica");
 
-           
+
             var empresa = new Empresa();
-            cliente.Empresa = empresa;
-            Console.WriteLine(cliente.Empresa.RazaoSocial);
+            //cliente.Empresa = empresa;
 
         }
-
-}
+        
+    }
 }
