8f7638afe9697d33302a6a9b9d1ea2c69ce4b5c3
Edgard <edgardpimentaa02@gmail.com>
2026-04-09 02:18:42 -0300

    refactor(entity): improve Empresa strcuture and relationships

--- CadastrodeClientes/Entities/Empresa.cs
+++ CadastrodeClientes/Entities/Empresa.cs
@@ -14,10 +14,14 @@ namespace CadastrodeClientes.Entities
         public string Cnpj { get; private set; } = string.Empty;
         public List<Cliente> Clientes { get; set; } = new List<Cliente>(); // Propriedade de navegação para os clientes associados à empresa
 
+
+
         /// <summary>
         /// Aplicando enum do TipoDeCliente para indicar o tipo de cliente (Pessoa Física ou Pessoa Jurídica) associado à empresa
         /// </summary>
         public TipoDeCliente TipoDeCliente { get; set; } = TipoDeCliente.PessoaFisica; // Aplicando o Enum TipoDeCliente para indicar o tipo de cliente (Pessoa Física ou Pessoa Jurídica)
+
+
 
         //Definindo o construtor RazaoSocial para a classe Empresa.
         public Empresa() { }
